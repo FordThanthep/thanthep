d548d02dcdfd3818e7901fd09a7a2ab8011e7063
FordThanthep <p0983848494@gmail.com>
2024-03-11 22:49:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript code (if needed)
+function login() {
+    // Add JavaScript code for login process
+    alert('Login button clicked!');
+    // Add additional code for login process
+}
+function register() {
+    // Add JavaScript code for registration process
+    alert('Register button clicked!');
+    // Add additional code for registration process
+}
